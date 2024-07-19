609a69e4ebe0d3b3d0efcba322a9a56ccc138dec
yashvardhann15 <yashvardhangupta318@gmail.com>
2024-07-20 01:27:26 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React , {useEffect} from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Footer from './Components/Footer/Footer'
-import { Outlet } from 'react-router-dom'
+import { Outlet , useNavigate} from 'react-router-dom'
 
 const App = () => {
+    const navigate = useNavigate(); // Use the useNavigate hook
+
+    useEffect(() => {
+        navigate('/'); // Navigate to the home page on component mount
+    }, [navigate]); 
+
     return (
         <div style={{
             background: 'linear-gradient(170deg to-right, black, dark-grey)', // Example gradient background
